14f1afd51482b7164cc43507ccc4af24b3ab5316
Cristian-Nastase <cristianemail@duck.com>
2026-01-13 15:30:50 +0200

    Am modificat scriptul dupa ce l-am refacut

--- nslookup.sh
+++ nslookup.sh
@@ -1,16 +1,20 @@
 #!/bin/bash
 
-if [ -z "$3" ];then
-	echo "Nu s-a transmis serverul DNS, va fi omis"
-	look=`nslookup $1`
-else
-	look=`nslookup $1 $3`	
-fi
+while read -r line; do
+    for word in $line; do
+        if [[ "$word" == \#* ]]; then
+            exit 0
+        fi
 
-last_word=$(echo "$look" | awk '{print $NF}' | tail -n 3 | head -n 1)
+        if [[ "$word" =~ ^[0-9] ]]; then
+            ip=$word
+        else
+            dns_ip=$(nslookup -query=A $word 1.1.1.1 | grep "Address:" | tail -n 1 | awk '{print $2}')
+            if [[ -z "$dns_ip" ]]; then continue; fi
 
-if [ "$last_word" != "$2" ]; then
-	echo "Bogus IP for $1"
-else
-	echo "Da, este corect"
-fi
+            if [[ "$dns_ip" != "$ip" ]]; then
+                echo "Bogus IP for $word in /etc/hosts!"
+            fi
+        fi
+    done
+done < /etc/hosts
